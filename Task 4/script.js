a96203793a89c747c127bb6e9860b8387fbe80b4
RokasV <vezikauskas.r@gmail.com>
2023-12-11 11:00:45 +0200

    task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,34 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const els = {
+  outputEl: document.getElementById("output"),
+};
+
+const ENDPOINT = "./cars.json";
+
+(function () {
+  fetch(ENDPOINT)
+    .then((resp) => resp.json())
+    .then((rez) => {
+      displayManufacturerCards(rez);
+    });
+})();
+
+function displayManufacturerCards(data) {
+  data.forEach((obj) => {
+    const oneCard = document.createElement("div");
+    oneCard.className = "class-card";
+    const modelsEl = obj.models;
+    const h2El = document.createElement("h2");
+    h2El.textContent = obj.brand;
+    const ulEl = document.createElement("ul");
+    oneCard.append(h2El, ulEl);
+    els.outputEl.append(oneCard);
+    modelsEl.forEach((carModel) => {
+      const liEl = document.createElement("li");
+      liEl.textContent = carModel;
+      ulEl.append(liEl);
+    });
+  });
+}
